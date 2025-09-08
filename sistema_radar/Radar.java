2da8ff1138ac427e8a55a3cc26f468a9d1e3b047
sabrinadiaas <sabrinadiasnog@gmail.com>
2025-09-08 09:50:45 -0300

    add get/set

--- sistema_radar/Radar.java
+++ sistema_radar/Radar.java
@@ -1,8 +1,29 @@
 package sistema_radar;
 
 public class Radar {
-    public String localizacao;
-    public Integer limiteVelocidade;
+    private String localizacao;
+    private Integer limiteVelocidade;
+
+    public Radar(String localizacao, Integer limiteVelocidade) {
+        this.localizacao = localizacao;
+        this.limiteVelocidade = limiteVelocidade;
+    }
+
+    public Integer getLocalizacao() {
+        return this.localizacao;
+    }
+
+    public void setlocalizacao(String localizacaoo) {
+        this.localizacao = localizacaoo;
+    }
+
+    public Integer getlimiteVelocidade() {
+        return this.limiteVelocidade;
+    }
+
+    public void setlimiteVelocidade(Integer limiteVelocidade) {
+        this.limiteVelocidade = limiteVelocidade;
+    }
 
     public void emitirNotificacao(String placa, Integer velocidadeObservada){
         System.out.println("Placa: " +placa);
@@ -12,8 +33,8 @@ public class Radar {
     }
 
     public void avaliarVelocidade(Carro carro){
-        if(carro.velocidade > this.limiteVelocidade){
-            emitirNotificacao(carro.placa, carro.velocidade);
+        if(carro.getVelocidade() > this.limiteVelocidade){
+            emitirNotificacao(carro.getPlaca(), carro.getVelocidade());
         }
     }
 }
--- sistema_radar/Simulacao.java
+++ sistema_radar/Simulacao.java
@@ -1,26 +1,19 @@
 package sistema_radar;
-
 public class Simulacao {
     public static void main(String[] args) {
-        Carro porsche = new Carro();
-        porsche.ano = 2026;
-        porsche.modelo = "Taycan";
-        porsche.placa = "LBM1501";
-        porsche.velocidade = 0;
+        Carro porsche = new Carro("LBM1501", 0, 2026, "Taycan");
 
-        Radar radar = new Radar();
-        radar.localizacao = "Pistão Sul";
-        radar.limiteVelocidade = 60;
+        Radar radar = new Radar("Pistão Sul", 60);
 
         radar.avaliarVelocidade(porsche);
 
+        porsche.acelerar(); //10
         porsche.acelerar();
         porsche.acelerar();
         porsche.acelerar();
         porsche.acelerar();
         porsche.acelerar();
-        porsche.acelerar();
-        porsche.acelerar();
+        porsche.acelerar(); //70
 
         radar.avaliarVelocidade(porsche);
 
